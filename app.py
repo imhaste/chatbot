cd53591edb65e2e59b745ab59f79e2004b55a9e4
imhaste <shubhamgoel2802@gmail.com>
2024-03-08 20:02:42 +0530

    Interface Completed

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
 import streamlit as st
 
-# Import your chatbot model or logic
-# from my_chatbot import Chatbot
+# App title
+st.set_page_config(page_title="swaadAI")
 
-# Create a Streamlit app
-def main():
-    st.title("Chatbot Interface")
+st.title('swaadAI')
+st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
 
-    # Initialize your chatbot
-    # chatbot = Chatbot()
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 
-    # Text input for user to enter messages
-    user_input = st.text_input("You:", "")
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
-    if st.button("Send"):
-        if(user_input):
-            print(user_input)
+# def clear_chat_history():
+#     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+# st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-if __name__ == "__main__":
-    main()
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = "This will be the response..."
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
